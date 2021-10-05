afd91db8f74b25641dd6b1fabc587c9b069be64c
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-10-05 08:41:32 +0100

    Remove commented codes as requested from the CR

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-import _ from 'lodash';
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, Now has imported new script
-  element.innerHTML = _.join([], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
 
 const data = [{
   index: 1,
