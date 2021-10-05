0c1fad17d16e3a7171c2c4adc5b5c8396fc7202a
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-10-05 09:25:12 +0100

    Add to inde.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('body');
+const data = [{
+        index: 1,
+        description: 'Attend a meeting',
+        completed: true,
+    }, {
+        index: 4,
+        description: 'Go shopping in the supermarket',
+        completed: true,
+    }, {
+        index: 2,
+        description: 'Go to the gym',
+        completed: false,
+    }, {
+        index: 3,
+        description: 'Go to the club and have some drinks',
+        completed: true,
+    },
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['hello', 'webpack'], ' ');
-    element.classList.add('hello');
+];
+let toDoItem = '';
+const itemList = document.querySelector('#list-container');
 
-    return element;
-}
-
-document.body.appendChild(component());
+data.forEach((item, index) => {
+    toDoItem += `
+    <li class="items" id="${index}">
+    <input type="checkbox" id="check-b" name="checkbox" value="">
+    <p id="item-list">${item.description}</p> <a href="#"><i id="fa-ellipsis-v" class="fas fa-ellipsis-v"></i></a>
+    </li>
+    <div class="line"></div>
+    `;
+});
+itemList.innerHTML = toDoItem;
