db17d9f71a737afe54501fb0a4c5d2f488775426
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-10-06 06:06:01 +0100

    Remove duplicate array from index.js file as recommended by CR

--- src/index.js
+++ src/index.js
@@ -3,35 +3,3 @@ import './style.css';
 import * as Task from './task.js';
 
 Task.displayTasks();
-const data = [{
-  index: 1,
-  description: 'Attend a meeting',
-  completed: true,
-}, {
-  index: 4,
-  description: 'Go shopping in the supermarket',
-  completed: true,
-}, {
-  index: 2,
-  description: 'Go to the gym',
-  completed: false,
-}, {
-  index: 3,
-  description: 'Go to the club and have some drinks',
-  completed: true,
-},
-
-];
-let toDoItem = '';
-const itemList = document.querySelector('#list-container');
-
-data.forEach((item, index) => {
-  toDoItem += `
-    <li class="items" id="${index}">
-    <input type="checkbox" id="check-b" name="checkbox" value="">
-    <p id="item-list">${item.description}</p> <a href="#"><i id="fa-ellipsis-v" class="fas fa-ellipsis-v"></i></a>
-    </li>
-    <div class="line"></div>
-    `;
-});
-itemList.innerHTML = toDoItem;
