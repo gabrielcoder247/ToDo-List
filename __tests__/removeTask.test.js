9683ae79dc8bc406f6a1b4d10174044898f6b6c7
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-10-14 14:20:41 +0100

    fix linter error

---
+++ __tests__/removeTask.test.js
@@ -0,0 +1,30 @@
+import localStorageMock from '../__mocks__/localStorage.js';
+import removeCompletedTask from '../__mocks__/removeTask.js';
+
+describe('Remove tasks', () => {
+  const arr = [{
+    description: 'something to do 1',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'something to do 2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'something to do 3',
+    completed: true,
+    index: 3,
+  },
+  ];
+  test('should return an array without completed tasks', () => {
+    expect(removeCompletedTask(arr)).toHaveLength(1);
+  });
+  test('should be updated after deleting the tasks', () => {
+    expect(localStorageMock.getItem('data')[0].description).toBe('something to do 2');
+  });
+  test('should be updated after deleting the tasks', () => {
+    expect(localStorageMock.getItem('data')[0].completed).toBe(false);
+  });
+});
