811182b0da3f81265f90eea9ccdf2e4a815fc72b
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-10-14 15:56:25 +0300

    Fix linter errors

--- __tests__/removeTask.test.js
+++ __tests__/removeTask.test.js
@@ -2,29 +2,29 @@ import localStorageMock from '../__mocks__/localStorage.js';
 import removeCompletedTask from '../__mocks__/removeTask.js';
 
 describe('Remove tasks', () => {
-    const arr = [{
-            description: 'watch a series',
-            completed: true,
-            index: 1,
-        },
-        {
-            description: 'Do my laundry',
-            completed: false,
-            index: 2,
-        },
-        {
-            description: 'Prepare my dinner',
-            completed: true,
-            index: 3,
-        },
-    ];
-    test('should return an array without completed tasks', () => {
-        expect(removeCompletedTask(arr)).toHaveLength(1);
-    });
-    test('should be updated after deleting the tasks', () => {
-        expect(localStorageMock.getItem('data')[0].description).toBe('something to do 2');
-    });
-    test('should be updated after deleting the tasks', () => {
-        expect(localStorageMock.getItem('data')[0].completed).toBe(false);
-    });
+  const arr = [{
+    description: 'watch a series',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Do my laundry',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Prepare my dinner',
+    completed: true,
+    index: 3,
+  },
+  ];
+  test('should return an array without completed tasks', () => {
+    expect(removeCompletedTask(arr)).toHaveLength(1);
+  });
+  test('should be updated after deleting the tasks', () => {
+    expect(localStorageMock.getItem('data')[0].description).toBe('something to do 2');
+  });
+  test('should be updated after deleting the tasks', () => {
+    expect(localStorageMock.getItem('data')[0].completed).toBe(false);
+  });
 });
